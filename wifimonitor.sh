7b64db2ebdb43b7eb1960e8b3eef1d2fd5381217
Angel Diaz <angel.diaz@gmail.com>
2019-03-07 17:51:44 +0100

    change description

--- wifimonitor.sh
+++ wifimonitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script prints the signal strength of the current WiFi
+# This script prints the signal strength of the WiFi you're currently connected to.
 
 while true
   do airport -I | grep agrCtlRSSI
