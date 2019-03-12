6a96e2874a1a67001fcb574401acf5efe56fa748
Angel Diaz <angel.diaz@gmail.com>
2019-03-12 10:49:16 +0100

    packet sniffer

--- packetsniffer.py
+++ packetsniffer.py
@@ -1,7 +1,19 @@
 #!/usr/local/bin/python3
 
+import os
 import scapy.all as scapy
+from scapy.layers import http
 
 
-def sniff():
-    
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    # Filter: arp, tcp, udp, port 22...
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
+
+# Launch script
+os.system('clear')
+sniff("en0")
