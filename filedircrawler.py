e9194bb687976d79aaadfc32fcb87dc2c99c665c
Angel Diaz <angel.diaz@gmail.com>
2019-03-13 09:31:40 +0100

    files and directories crawl

--- filedircrawler.py
+++ filedircrawler.py
@@ -13,7 +13,7 @@ def get_arguments():
     # Parse the passed arguments
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-t", "--target", dest="target", help="Website to be analyzed (ex: example.com)")
+    parser.add_argument("-t", "--target", dest="target", help="Subdomain to be analyzed (ex: www.example.com)")
 
     arguments = parser.parse_args()
 
@@ -25,7 +25,7 @@ def get_arguments():
 
 def request(url):
     try:
-        return requests.get("http://" + url)
+        return requests.get(url)
     except requests.exceptions.ConnectionError:
         pass
     except requests.exceptions.InvalidURL:
@@ -35,23 +35,24 @@ def request(url):
 
 options             = get_arguments()
 target_url          = options.target
-valid_subdomains    = []
+valid_filedirs      = []
 
 try:
-    with open("wordlists/subdomains-list.txt", "r") as subdomains_list:
+    with open("wordlists/files-and-dirs-list.txt", "r") as subdomains_list:
         for line in subdomains_list:
             # time.sleep(0.1)
-            subdomain   = line.strip() + "." + target_url
-            response    = request(subdomain)
-            if response:
-                valid_subdomains.append(subdomain)
-                print("\n[+] Discovered subdomain: " + subdomain)
+            filedir     = target_url + "/" + line.strip()
+            response    = request(filedir)
+            if response and response.status_code != 300:
+                print(response.status_code)
+                valid_filedirs.append(filedir)
+                print("\n[+] Discovered file or directory: " + filedir)
             else:
                 sys.stdout.write(".")
                 sys.stdout.flush()
-        print(valid_subdomains)
+        print(valid_filedirs)
 
 except KeyboardInterrupt:
     print("\n")
-    print(valid_subdomains)
-    print ("\n[-] Quitting subdomaincrawler.py")
+    print(valid_filedirs)
+    print ("\n[-] Quitting filedircrawler.py")
